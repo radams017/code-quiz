ced7ac20c7c0bdbfdfb5fc95021e6ad5a502eda9
Ryan Adams <ryanadams14@gmail.com>
2021-06-01 22:02:06 -0500

    added questions

--- assets/questions.js
+++ assets/questions.js
@@ -5,8 +5,23 @@ var questions = [
         answer: "[1,2,3]"
     },
     {
-        title: "sdfsdf",
-        choices: ["sdfs", "gdfg", "erge"],
-        answer: "sdfs"
+        title: "What Does CSS Stand For",
+        choices: ["Chrome Script School", "Cascading Style Sheets", "Core Server Side", "Client Side Storage"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "What Is '.remove()' An Example Of",
+        choices: ["Method", "Tag", "Assignment Operator", "Comparison Operator"],
+        answer: "Method"
+    },
+    {
+        title: "What Should You Add To The End of Each Line in Javascript",
+        choices: [",", ":", ";", "|"],
+        answer: ";"
+    },
+    {
+        title: "What Is NOT An Example of A Javascript Library",
+        choices: ["Node.js", "Vue.js", "Skad.js", "JQuery"],
+        answer: "Skad.js"
     }
 ]
